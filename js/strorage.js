8291a176542c635666654988de75d1061ad3b00c
Rizkyaditiya21 <rizkyaditiyafernando@gmail.com>
2026-02-16 12:51:04 +0700

    Update strorage.js

--- js/strorage.js
+++ js/strorage.js
@@ -1,16 +1,4 @@
-function getUsers(){
- return JSON.parse(localStorage.getItem("users")) || [];
+function logout(){
+ localStorage.removeItem("user");
+ window.location.href = "login.html";
 }
-
-function saveUsers(users){
- localStorage.setItem("users", JSON.stringify(users));
-}
-
-function getCurrentUser(){
- return JSON.parse(localStorage.getItem("currentUser"));
-}
-
-function setCurrentUser(user){
- localStorage.setItem("currentUser", JSON.stringify(user));
-}
-
