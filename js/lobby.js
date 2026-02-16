05897fc7b1de525a40208d6f597c0a6235ba8a3c
Rizkyaditiya21 <rizkyaditiyafernando@gmail.com>
2026-02-16 12:27:36 +0700

    Update lobby.js

--- js/lobby.js
+++ js/lobby.js
@@ -1,1 +1,26 @@
+updateBalanceDisplay();
+
+fetch("data/config.json")
+.then(res=>res.json())
+.then(data=>{
+
+ let container = document.getElementById("gameList");
+
+ data.games.forEach(game=>{
+
+  container.innerHTML += `
+   <div class="game-card"
+    onclick="openGame('${game.folder}')">
+
+    🎰 ${game.name}
+
+   </div>
+  `;
+ });
+
+});
+
+function openGame(folder){
+ window.location = `games/${folder}/game.html`;
+}
 
