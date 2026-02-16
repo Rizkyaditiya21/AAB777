8412aca8f122650f6e9deaf961b7d79d19e88957
Rizkyaditiya21 <rizkyaditiyafernando@gmail.com>
2026-02-16 13:11:25 +0700

    Update game.js

--- games/mahjong/game.js
+++ games/mahjong/game.js
@@ -1,33 +1,65 @@
-let engine = new SlotEngine(
- ["🐉","🀄","💎","🍀","⭐"]
-);
+updateBalanceDisplay();
+
+const BET = 500;
+
+function drawReels(reels){
+
+ let container = document.getElementById("reels");
+ container.innerHTML = "";
+
+ reels.forEach(reel=>{
+
+  let div = document.createElement("div");
+  div.className = "reel";
+  div.innerText = reel[1];
+
+  container.appendChild(div);
+
+ });
+}
+
+
 
 function spin(){
 
- updateBalance(-10);
+ let balance = parseInt(localStorage.getItem("balance"));
 
- let result = engine.spin();
-
- show(result);
-
- let win = checkWin(result);
-
- if(win>0){
-  updateBalance(win);
-  resultText.innerText = "WIN "+win;
- }else{
-  resultText.innerText = "LOSE";
+ if(balance < BET){
+  alert("Saldo kurang");
+  return;
  }
 
+ balance -= BET;
+ localStorage.setItem("balance",balance);
+ updateBalanceDisplay();
+
+
+ let reels = spinEngine();
+
+ drawReels(reels);
+
+ let result = checkWin(reels);
+
+ if(result.win > 0){
+
+  balance += result.win;
+  localStorage.setItem("balance",balance);
+
+  document.getElementById("result").innerText =
+  "MENANG : " + result.win;
+
+  updateBalanceDisplay();
+ }
+
+
+ if(result.freeSpin){
+  setTimeout(()=>{
+
+   document.getElementById("result").innerText =
+   "FREE SPIN !!!";
+
+   spin();
+
+  },1500);
+ }
 }
-
-function show(result){
-
- let reels = document.querySelectorAll(".reel");
-
- reels.forEach((reel,i)=>{
-  reel.innerText = result[i].join("\n");
- });
-
-}
-
