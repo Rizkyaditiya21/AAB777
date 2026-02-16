3483f3d35864d80efd446200e8ccce913257e80a
Rizkyaditiya21 <rizkyaditiyafernando@gmail.com>
2026-02-16 12:27:03 +0700

    Update balance.js

--- js/balance.js
+++ js/balance.js
@@ -1,1 +1,29 @@
+function updateBalanceDisplay(){
+
+ let user = getCurrentUser();
+
+ if(!user){
+   window.location="login.html";
+ }
+
+ document.getElementById("balance").innerText =
+ "💰 "+user.balance+" coins";
+}
+
+function updateBalance(amount){
+
+ let users = getUsers();
+ let user = getCurrentUser();
+
+ user.balance += amount;
+
+ let index = users.findIndex(
+  u=>u.username===user.username
+ );
+
+ users[index] = user;
+
+ saveUsers(users);
+ setCurrentUser(user);
+}
 
