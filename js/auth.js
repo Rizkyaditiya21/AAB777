0d61abd42eda075492a97b0473c91c2e479d5e15
Rizkyaditiya21 <rizkyaditiyafernando@gmail.com>
2026-02-16 12:26:43 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,42 @@
+function register(username,password){
+
+ let users = getUsers();
+
+ if(users.find(u=>u.username===username)){
+   alert("User sudah ada");
+   return;
+ }
+
+ users.push({
+   username,
+   password,
+   balance:1000
+ });
+
+ saveUsers(users);
+ alert("Register sukses");
+ window.location="login.html";
+}
+
+function login(username,password){
+
+ let users = getUsers();
+
+ let user = users.find(
+  u=>u.username===username && u.password===password
+ );
+
+ if(!user){
+  alert("Login gagal");
+  return;
+ }
+
+ setCurrentUser(user);
+ window.location="index.html";
+}
+
+function logout(){
+ localStorage.removeItem("currentUser");
+ window.location="login.html";
+}
 
