edf651b26ab59156b9c953247def9cf4440b9b70
Rizkyaditiya21 <rizkyaditiyafernando@gmail.com>
2026-02-16 12:30:37 +0700

    Update game.js

--- games/mahjong/game.js
+++ games/mahjong/game.js
@@ -1,1 +1,33 @@
+let engine = new SlotEngine(
+ ["🐉","🀄","💎","🍀","⭐"]
+);
+
+function spin(){
+
+ updateBalance(-10);
+
+ let result = engine.spin();
+
+ show(result);
+
+ let win = checkWin(result);
+
+ if(win>0){
+  updateBalance(win);
+  resultText.innerText = "WIN "+win;
+ }else{
+  resultText.innerText = "LOSE";
+ }
+
+}
+
+function show(result){
+
+ let reels = document.querySelectorAll(".reel");
+
+ reels.forEach((reel,i)=>{
+  reel.innerText = result[i].join("\n");
+ });
+
+}
 
